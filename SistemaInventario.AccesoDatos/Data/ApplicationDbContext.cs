43d3dfa9772f4e7a0e4756de7eaae39b318d6822
Herberts Aldava Asencios <h.gaia@live.com>
2023-06-12 12:02:50 -0500

    Arquitectura y configuraciones iniciales

---
+++ SistemaInventario.AccesoDatos/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SistemaInventario.Modelos.Admin;
+using System.Reflection;
+
+namespace SistemaInventario.AccesoDatos.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Entidades de nuestra BD
+        public DbSet<Bodega> Bodegas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ SistemaInventario.Modelos/Admin/Bodega.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos.Admin
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="{0} es obligatorio.")]
+        [MaxLength(60, ErrorMessage ="{0} no debe contener más de {1} carácteres.")]
+        public string? Nombre { get; set; }
+
+        [Required(ErrorMessage = "{0} es obligatorio.")]
+        [MaxLength(10, ErrorMessage = "{0} no debe contener más de {1} carácteres.")]
+        public string? Descripcion { get;}
+
+        [Required(ErrorMessage ="{0} es obligatorio.")]
+        public bool Estado { get; set; }
+    }
+}
